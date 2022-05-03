9322d2641aa711329d58ca243a228a3ea4889696
Lígia Santos <83599167+Li-hub-san@users.noreply.github.com>
2022-05-03 17:28:45 +0100

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
+"use strict";
+
 const pokemonListUrl = "https://pokeapi.co/api/v2/pokemon?limit=150";
+const cardContainer = document.querySelector(".card-container");
+
+const getJson = function (url, errMsg = "Something went wrong") {
+  return fetch(url).then((response) => {
+    if (!response.ok) throw new Error(`${errMsg} ${response.status}`);
+
+    return response.json();
+  });
+};
+
+getJson(pokemonListUrl)
+  .then((resp) => {
+    const pokemonList = resp["results"];
+    console.log("list", pokemonList);
+
+    // pokemonList.forEach((el) => {
+    //   console.log(el);
+    //   cardContainer.insertAdjacentHTML(
+    //     "beforeend",
+    //     `<img src=${el.image}><p>${el.name}</p>`
+    //   );
+    // });
+
+    const {
+      results: [{ name: name, url: image }],
+    } = resp;
+
+    console.log(name);
+    console.log(image);
+
+    return fetch(image);
+  })
+  .then((response) => response.json())
+  .then((resp) => {
+    console.log("trying name", resp);
+
+    const { sprites, name } = resp;
+
+    cardContainer.insertAdjacentHTML(
+      "afterbegin",
+      `<p class="card-name">${name}</p>
+          <img alt="pokemon ${name}" class="card-image" src=${sprites["front_default"]}>`
+    );
+  });
